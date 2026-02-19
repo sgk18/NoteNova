014fc32949703117211650542df42057bea253f7
Suryachalam V M <suryachalam18@gmail.com>
2026-02-19 10:03:49 +0530

    app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.notenova.app',
   appName: 'NoteNova',
-  webDir: 'out',
+  webDir: 'www',
   server: {
     url: 'https://note-nova-khaki.vercel.app', // 👈 PUT YOUR REAL VERCEL LINK
     cleartext: true
